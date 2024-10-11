d2210f52a1065ccabd21be346ca2d4295a6ebcc9
Ali Osman <lsmnkayhan@gmail.com>
2024-10-11 11:26:53 +0300

    Quick

--- todo/app.js
+++ todo/app.js
@@ -1,34 +1,44 @@
-// Selectors
-const cardbody2 = document.querySelectorAll(".card-body")[1];
-const todoList = document.querySelector(".list-group");
-let todos = document.querySelectorAll(".list-group-item");
+// // Selectors
+// const cardbody2 = document.querySelectorAll(".card-body")[1];
+// const todoList = document.querySelector(".list-group");
+// const todos = document.querySelectorAll(".list-group-item");
 
-// New Elements
-    // Element Button
-const link = document.createElement("a")
-link.id = "link";
-link.className = "btn btn-dark btn-sm mt-3";
-link.innerHTML = "My site";
-link.href = "#";
-link.target = "_blank";
+// // New Elements
+//     // Element Button
+// const link = document.createElement("a")
+// link.id = "link";
+// link.className = "btn btn-dark btn-sm mt-3";
+// link.innerHTML = "My site";
+// link.href = "#";
+// link.target = "_blank";
 
-    // Element Todo
-const todo = document.createElement("li")
-todo.className = "list-group-item d-flex justify-content-between";
-todo.innerHTML =  "Merhaba";
+//     // Element Todo
+// const todo = document.createElement("li")
+// todo.className = "list-group-item d-flex justify-content-between";
+// todo.innerHTML =  "Merhaba";
 
-const deleteTodo = document.createElement("a");
-deleteTodo.href = "#";
+// const deleteTodo = document.createElement("a");
+// deleteTodo.href = "#";
 
-const deleteIcon = document.createElement("i");
-deleteIcon.className = "fa fa-remove";
+// const deleteIcon = document.createElement("i");
+// deleteIcon.className = "fa fa-remove";
 
-todo.appendChild(deleteTodo.appendChild(deleteIcon));
+// todo.appendChild(deleteTodo.appendChild(deleteIcon));
 
-    // Append Final element
-todoList.appendChild(todo);
+//     // Append Final element
+// todoList.appendChild(todo);
 
-cardbody2.appendChild(link);
+// cardbody2.appendChild(link);
+
+// console.log(todos);
 
 
-console.log(todos);
+const todoName = document.querySelector("#todoName")
+
+const todo = addEventListener("keyup", )
+
+
+
+console.log(todoName.)
+
+
