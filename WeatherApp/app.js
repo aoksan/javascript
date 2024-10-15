58e386ec5993d510941f82500708150fc580273b
aoksan <116678713+aoksan@users.noreply.github.com>
2024-10-15 13:36:12 +0300

    Frontend Finished

--- WeatherApp/app.js
+++ WeatherApp/app.js
@@ -2,3 +2,18 @@ const apiKey = 'e186ad6a3dfd570c47cac2780b4b6d96';
 
 
 
+
+
+
+
+{/* <div id="status" class="col">
+<div class="card mt-3">
+  <div class="card-header text-bg-success justify-content-between d-flex">City 
+    <button type="button" class="btn-close" disabled aria-label="Close"></button>
+  </div>
+  <ul class="list-group list-group-flush list-group-item bg-light">
+    <li class="list-group-item bg-light">Weather - Temp</li>
+    <li class="list-group-item bg-light">Sunrise - Sunset</li>
+  </ul>
+</div>
+</div> */}
